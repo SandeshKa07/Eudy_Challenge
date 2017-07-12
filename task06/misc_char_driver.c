fa5b93fe37959547a9d13c312a44145e6b5354a0
SandeshKa07 <kas.sandesh@gmail.com>
2017-07-12 10:39:47 +0530

    [task 06] added source files of task 06 of eudy challenge
    Signed-off-by: SandeshKa07 <kas.sandesh@gmail.com>

---
+++ task06/misc_char_driver.c
@@ -0,0 +1,69 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/printk.h>
+
+#define myassignID "a89cef62eae7"
+#define idLen 13
+
+static ssize_t my_misc_read(struct file *file, char __user *buffer,
+			    size_t count, loff_t *ppos)
+{
+	return simple_read_from_buffer(buffer, count, ppos,
+				       myassignID, sizeof(myassignID));
+}
+
+static ssize_t my_misc_write(struct file *file, const char __user *buffer,
+			     size_t count, loff_t *ppos)
+{
+	char wbuffer[idLen];
+
+	if (count != idLen)
+		return -EINVAL;
+
+	if (copy_from_user(wbuffer, buffer, count))
+		return -EFAULT;
+
+	if (strncmp(myassignID, wbuffer, (idLen - 1)))
+		return -EINVAL;
+
+	return count;
+}
+
+static const struct file_operations my_misc_fops = {
+	.owner = THIS_MODULE,
+	.read = my_misc_read,
+	.write = my_misc_write,
+};
+
+static struct miscdevice my_misc_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "eudyptula",
+	.fops = &my_misc_fops,
+};
+
+static int __init my_misc_init(void)
+{
+	int error;
+
+	pr_info("\nHello World\n");
+	error = misc_register(&my_misc_device);
+	if (error) {
+		pr_err("\n Error Registering the misc device\n");
+		return error;
+	}
+	return 0;
+}
+
+static void __exit my_misc_exit(void)
+{
+	misc_deregister(&my_misc_device);
+	pr_info("Good Bye\n");
+}
+
+module_init(my_misc_init);
+module_exit(my_misc_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sandesh K A");
