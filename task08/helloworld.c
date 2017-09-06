c6905d5631e04141b3604fabef8ca7123ce29815
SandeshKa07 <kas.sandesh@gmail.com>
2017-09-06 14:28:48 +0530

    [debugfs] Added misc char driver functionality from task06
    Signed-off-by: SandeshKa07 <kas.sandesh@gmail.com>

--- task08/helloworld.c
+++ task08/helloworld.c
@@ -1,20 +1,65 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/debugfs.h>
+#include <linux/miscdevice.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/printk.h>
+
+#define myassignID "a89cef62eae7"
+#define idLen 13
+
+static struct dentry *my_dir;
+static struct dentry *my_id_file;
+
+static ssize_t my_misc_read(struct file *file, char __user *buffer,
+			    size_t count, loff_t *ppos)
+{
+	return simple_read_from_buffer(buffer, count, ppos,
+				       myassignID, sizeof(myassignID));
+}
+
+static ssize_t my_misc_write(struct file *file, const char __user *buffer,
+			     size_t count, loff_t *ppos)
+{
+	char wbuffer[idLen];
+	ssize_t retval;
+
+	if (count != idLen)
+		return -EINVAL;
+
+	retval = simple_write_to_buffer(wbuffer, sizeof(myassignID),
+					ppos, buffer, count - 1);
+
+	if (retval < 0)
+		return retval;
+
+	if (strncmp(myassignID, wbuffer, (idLen - 1)))
+		return -EINVAL;
+
+	return count;
+}
+
+static const struct file_operations my_misc_fops = {
+	.owner = THIS_MODULE,
+	.read = my_misc_read,
+	.write = my_misc_write,
+};
 
 static int __init module_startup(void)
 {
 	my_dir = debugfs_create_dir("eudyptula", NULL);
-	if(!my_dir) {
-		printk("Failed to create the directory under /debugfs\n");
+	if (!my_dir) {
+		pr_info("Failed to create the directory under /debugfs\n");
 		return -ENODEV;
 	}
-	my_id_file = debugfs_create_file("id", 0666, my_dir, myassignID, &my_misc_fops);
-	if(!my_id_file) {
-		printk("Failed to create a file under /debugfs/eudyptula\n");
+	my_id_file = debugfs_create_file("id", 0666, my_dir, NULL,
+					 &my_misc_fops);
+	if (!my_id_file) {
+		pr_info("Failed to create a file under /debugfs/eudyptula\n");
 		return -ENODEV;
 	}
-		
+
 	return 0;
 }
 
